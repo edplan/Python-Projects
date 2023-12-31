33c184b34f67f99066aee31e6d13ce9538a0d540
edplan <154573590+edplan@users.noreply.github.com>
2023-12-30 17:44:48 -0800

    Testing the program and making sure I have the correct extensions

---
+++ CS161/Project-1.py
@@ -0,0 +1,7 @@
+# Author: Eddi F. Miranda-Perez
+# GitHub username: edplan
+# Date: Mar, 27, 2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fav_animal = input("Please enter your favorite animal: ")
