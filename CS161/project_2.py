041bde8626a9e1f58b7c991916b33e3b9a9cb214
edplan <154573590+edplan@users.noreply.github.com>
2023-12-31 20:12:06 -0800

    Completed Code
    Removed all user input variables and used a list instead with a while loop. The while loop will iterate until five values have been entered. The list is then summed and divided by 5 to output the average.

---
+++ CS161/project_2.py
@@ -0,0 +1,14 @@
+# Author: Eddi F. Miranda-Perez
+# GitHub username: edplan
+# Date: Apr, 06, 2022
+# Description: Asks the user for five numbers and then 
+# .............prints out the average of those numbers.
+
+num_list = []
+
+while len(num_list) != 5:
+    print("Please enter 5 numbers: ")
+    num1 = float(input())
+    num_list.append(num1)
+    
+print(sum(num_list) / 5)
